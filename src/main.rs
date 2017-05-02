13acf7d1f3d4c0221cc03ebecc3769b11e35f950
Aleksa Sarai <cyphar@cyphar.com>
2017-05-02 14:35:06 +1000

    *: add inital cargo setup
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+/*
+ * whistled: generic GNU/Linux application firewall daemon
+ * Copyright (C) 2017 Aleksa Sarai <cyphar@cyphar.com>
+ *
+ * whistled is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * whistled is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with whistled.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
